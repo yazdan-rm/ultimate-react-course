875348f83a407c44ba90ac1ec394384abb298d02
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-02-11 19:42:24 +0330

    creat init file with create-react-app commands

--- 03-pizza-menu/starter/profile-card-v1/src/App.js
+++ 03-pizza-menu/starter/profile-card-v1/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="card">
+            <Avatar/>
+            <div className='data'>
+                <Intro/>
+                <SkillList/>
+            </div>
+        </div>
+    );
 }
+
+function Avatar() {
+    return (<img src='logo.png' className="avatar" alt="yazdan rafieemanesh"/>)
+}
+
+function Intro() {
+    return (<div>
+        <h1>Yazdan Rafiee Manesh</h1>
+        <p>I'm hungry for learn new things in react, I currently work on company as backend developer.</p>
+    </div>)
+}
+
+function SkillList() {
+    return (
+        <div className="skill-list">
+            <Skill skill={'React'} emoji={'💯'} color={'green'}/>
+            <Skill skill={'HTML+CSS'} emoji={'💯'} color={'red'}/>
+            <Skill skill={'JS'} emoji={'💯'} color={'yellow'}/>
+            <Skill skill={'JAVA'} emoji={'💯'} color={'orange'}/>
+        </div>
+    )
+}
+
+function Skill(props) {
+    return (
+        <div className="skill" style={{backgroundColor: props.color}}>
+            <span>{props.skill}</span>
+            <span>{props.emoji}</span>
+        </div>
+    )
+}
+
 
 export default App;
