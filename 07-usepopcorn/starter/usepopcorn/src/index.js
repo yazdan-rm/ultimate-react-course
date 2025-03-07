a3fae1059a4928adc5f4e9a1b722b7e54843ed00
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-03-07 16:51:21 +0330

    create accordion item

--- 07-usepopcorn/starter/usepopcorn/src/index.js
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import StarRating from "./StarRating";
+// import './index.css';
+// import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <App/>
+      <StarRating maxRating={'5'}/>
+      <StarRating maxRating={5}/>
+      <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+      <StarRating maxRating={5} defaultRating={2}/>
+      <StarRating size={24} color={'red'}/>
     </React.StrictMode>
 );
