7862f1bfc4576d1434a94c0352f0cf21312fe50f
Yazdan <1yazdanrafieemanesh@gmail.com>
2025-03-19 02:03:28 +0330

    finish intermediate project

---
+++ 07-usepopcorn/starter/usepopcorn/src/useLocalStorageState.js
@@ -0,0 +1,14 @@
+import {useEffect, useState} from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
+
+  useEffect(function () {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value]);
+
+  return [value, setValue];
+}
